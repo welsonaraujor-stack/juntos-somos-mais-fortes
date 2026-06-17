614ceda0b4091055f068e29c7f8dd18d66bf1bf3
welaon ribeiro <welson.araujor@gmail.com>
2026-06-17 01:14:06 -0300

    Página Grupos de pessoas

---
+++ grupo/script.js
@@ -0,0 +1,13 @@
+const botao =
+document.getElementById("btnGrupo");
+
+botao.addEventListener("click", () => {
+
+    alert(
+        "Solicitação enviada com sucesso!"
+    );
+
+    // Futuro:
+    // enviar dados para banco de dados
+
+});
