71ca6977ad75edd0d794ce9f6290417e1eabdc52
welaon ribeiro <welson.araujor@gmail.com>
2026-06-17 01:03:42 -0300

    Página de pessoa

---
+++ pessoa/script.js
@@ -0,0 +1,15 @@
+const botao =
+document.getElementById("btnTriagem");
+
+botao.addEventListener("click", () => {
+
+    alert(
+        "Indo para a Pré-Triagem..."
+    );
+
+    // Futuro
+
+    // window.location.href =
+    // "pre-triagem.html";
+
+});
