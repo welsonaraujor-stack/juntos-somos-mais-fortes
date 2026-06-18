614cd1fa49dd24bba9a97c3e54209113156efda5
welaon ribeiro <welson.araujor@gmail.com>
2026-06-17 23:59:14 -0300

    Página instituição

---
+++ instituicao/script.js
@@ -0,0 +1,13 @@
+const botao =
+document.getElementById("btnInstituicao");
+
+botao.addEventListener("click", () => {
+
+    alert(
+        "Campanha cadastrada com sucesso!"
+    );
+
+    // Futuramente:
+    // integração com banco de dados
+
+});
