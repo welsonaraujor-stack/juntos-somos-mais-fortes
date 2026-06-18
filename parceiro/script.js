6273c418f4db29f67ba7dfaf1a30059392919801
welaon ribeiro <welson.araujor@gmail.com>
2026-06-18 00:47:59 -0300

    Página Parceiro

---
+++ parceiro/script.js
@@ -0,0 +1,13 @@
+const botao =
+document.getElementById("btnParceiro");
+
+botao.addEventListener("click", () => {
+
+    alert(
+        "Cadastro de parceiro realizado com sucesso!"
+    );
+
+    // Futuro:
+    // integração com banco de dados
+
+});
