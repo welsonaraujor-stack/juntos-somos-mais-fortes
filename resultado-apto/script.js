0bcde890d864b657c54610d8314ce28dff6fb0be
welaon ribeiro <welson.araujor@gmail.com>
2026-06-20 01:18:58 -0300

    Resultado

---
+++ resultado-apto/script.js
@@ -0,0 +1,13 @@
+function abrirMapa(){
+
+    window.location.href =
+    "../mapa-unidades/index.html";
+
+}
+
+function agendarDoacao(){
+
+    window.location.href =
+    "../agendamento/index.html";
+
+}
