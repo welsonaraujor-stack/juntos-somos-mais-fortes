489911f099ca84d29fc684610c1646681bbdcde7
welaon ribeiro <welson.araujor@gmail.com>
2026-06-20 12:34:23 -0300

    Página de confirmação

---
+++ confirmacao/script.js
@@ -0,0 +1,14 @@
+function adicionarAgenda(){
+
+    alert(
+    "Função de calendário será implementada futuramente."
+    );
+
+}
+
+function voltarInicio(){
+
+    window.location.href =
+    "../resultado-apto/index.html";
+
+}
