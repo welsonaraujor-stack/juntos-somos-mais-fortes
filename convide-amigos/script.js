68b1ab4ffee33ce4b2c5580ffafb9a7185cd1cbc
welaon ribeiro <welson.araujor@gmail.com>
2026-06-20 22:48:42 -0300

    Página de convidar amigos

---
+++ convide-amigos/script.js
@@ -0,0 +1,19 @@
+function copiarCodigo(){
+
+    navigator.clipboard.writeText(
+        "JUNTOS2026"
+    );
+
+    alert(
+    "Código copiado!"
+    );
+
+}
+
+function compartilhar(){
+
+    alert(
+    "Compartilhamento será conectado futuramente."
+    );
+
+}
