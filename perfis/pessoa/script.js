d77071d72da9d261518b4b4f555f3ab57c40b2c2
welaon ribeiro <welson.araujor@gmail.com>
2026-06-23 13:18:25 -0300

    Comunicação do login, pre cadrastro e pessoa automatico ok

--- perfis/pessoa/script.js
+++ perfis/pessoa/script.js
@@ -1,3 +1,73 @@
+// =============================
+// RECUPERAR DADOS DO CADASTRO
+// =============================
+
+const nome =
+localStorage.getItem("nome");
+
+const cpf =
+localStorage.getItem("cpf");
+
+const nascimento =
+localStorage.getItem("nascimento");
+
+const telefone =
+localStorage.getItem("telefone");
+
+const cidade =
+localStorage.getItem("cidade");
+
+const email =
+localStorage.getItem("usuario");
+
+if(nome){
+
+    document.getElementById(
+    "nomeCompleto"
+    ).value = nome;
+
+}
+
+if(cpf){
+
+    document.getElementById(
+    "cpfUsuario"
+    ).value = cpf;
+
+}
+
+if(nascimento){
+
+    document.getElementById(
+    "dataNascimento"
+    ).value = nascimento;
+
+}
+
+if(telefone){
+
+    document.getElementById(
+    "telefoneUsuario"
+    ).value = telefone;
+
+}
+
+if(cidade){
+
+    document.getElementById(
+    "cidadeUsuario"
+    ).value = cidade;
+
+}
+
+if(email){
+
+    document.getElementById(
+    "emailUsuario"
+    ).value = email;
+
+}
+
 const botao =
 document.getElementById("btnTriagem");
 
